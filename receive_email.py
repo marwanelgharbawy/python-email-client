0f77adbbff64594aff9a65ea22789a2b9399af5a
Marwan <marwanelgharbawy2@gmail.com>
2026-03-13 15:49:11 +0200

    Parse body from email

--- receive_email.py
+++ receive_email.py
@@ -2,22 +2,51 @@ import imaplib
 import email
 import os
 from email.header import decode_header
+from re import search
 from dotenv import load_dotenv
 
-load_dotenv()
+# def get_body(msg):
+#     if msg.is_multipart():
+#         return get_body(msg.get_payload(0))
+#     else:
+#         return msg.get_payload(None, True)
 
-def fetch_latest_email(email, password):
+def fetch_latest_email(email_address, password):
     try:   
         # connect to IMAP server and login
         # modify the server address depending on the recepient (ex: imap.yahoo.com)
         con = imaplib.IMAP4_SSL("imap.gmail.com")
-        con.login(email, password)
+        con.login(email_address, password)
         con.select("Inbox")
         
         print("Connected successfully to the email server.")
         
+        status, messages = con.search(None, 'ALL')
         
+        # "messages" contain a list of email ids -> split to get ids
+        mail_ids = messages[0].split()
+        if mail_ids:
+            # get last email id and fetch the email using RFC822 protocol
+            latest_id = mail_ids[-1]
+            status, msg_data = con.fetch(latest_id, '(RFC822)')
+            
+            for response_part in msg_data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_bytes(response_part[1])
+                    
+                    if msg.is_multipart():
+                        body = msg.get_payload(0).get_payload(decode=True).decode(errors='ignore')
+                    else:
+                        body = msg.get_payload(decode=True).decode(errors='ignore')
+                        
+                    print(f"Body:\n{body}")
+        else:
+            print("No emails found.")
         
+        print(messages)
+        
+        con.close()
+        con.logout()
         return True
 
     except imaplib.IMAP4.error as e:
@@ -30,8 +59,8 @@ def fetch_latest_email(email, password):
 if __name__ == "__main__":
     load_dotenv()
     # test
-    email_address = os.getenv("EMAIL_ADDRESS")
-    email_password = os.getenv("EMAIL_PASSWORD")
+    email_address = os.getenv("RECIPIENT_ADDRESS")
+    email_password = os.getenv("RECIPIENT_PASSWORD")
     
     if fetch_latest_email(email_address, email_password):
         print("Email fetching process completed.")
